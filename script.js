b5d7ddfc438be74f0ab1a2eb88250dab399f39d6
Glitch (hello-webpage) <none>
2020-07-29 11:08:52 +0000

    👽💂 Checkpoint ./script.js:18108127/163:18108132/342:18108380/566 ./style.css:18108132/278:18108380/203 ./.glitch-assets:18108127/6516

--- script.js
+++ script.js
@@ -1,36 +1,44 @@
 let imgSrc = [
   { 
     pairId: 0,
-    src: "patata",
+    src: "https://cdn.glitch.com/56d96ce9-5171-477f-8560-558ec3af0051%2Fclover-a.png?v=1596020454798",
   }, 
   { 
     pairId: 1,
-    src: "patata",
+    src: "https://cdn.glitch.com/56d96ce9-5171-477f-8560-558ec3af0051%2Fdiamonds-a.PNG?v=1596020457628",
   },
   { 
     pairId: 2,
-    src: "patata",
+    src: "https://cdn.glitch.com/56d96ce9-5171-477f-8560-558ec3af0051%2Fclover-9.png?v=1596020745882",
+  },
+   { 
+    pairId: 3,
+    src: "https://cdn.glitch.com/56d96ce9-5171-477f-8560-558ec3af0051%2Fspades-a.PNG?v=1596020468854",
   }, 
 ];
+
 
 let imgPairs = doubleContent(imgSrc);
 let shuffledImgs = shuffle(imgPairs);
 
+let imgsGrid = document.getElementById("images-grid");
 
-
+shuffledImgs.forEach((img) => {
+  let imgDiv = createImgDiv(img);
+  imgsGrid.appendChild(imgDiv);
+})
 
 function createImgDiv(img) {
   let container = document.createElement("div");
   container.setAttribute("data-pair", img.pairId);
   container.style.backgroundImage = img.src;
-  
+  container.classList.add("cell");
   return container;
 }
 
 function doubleContent(arr) {
   return arr.concat(arr);
 }
-
 
 function shuffle(array) {
   let newArray = Array.from(array);
