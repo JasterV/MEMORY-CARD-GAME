0ce372d861641310e204ad9f487c9ca7ed900567
Glitch (hello-webpage) <none>
2020-07-29 10:46:09 +0000

    🌋🌿 Checkpoint ./script.js:18108127/227:18108132/3129:18108380/537

--- script.js
+++ script.js
@@ -4,7 +4,19 @@ let imgSrc = [
     src: "patata",
   }, 
   { 
+    pairId: 0,
+    src: "patata",
+  }, 
+  { 
     pairId: 1,
+    src: "patata",
+  }, 
+  { 
+    pairId: 1,
+    src: "patata",
+  }, 
+  { 
+    pairId: 2,
     src: "patata",
   }, 
   { 
@@ -14,23 +26,34 @@ let imgSrc = [
 ];
 
 
+let shuffledImgs = 
 
 
-function createImgsGrid(imgs) {
-  let board = [
-    ["", "", "", ""],
-    ["", "", "", ""],
-    ["", "", "", ""],
-    ["", "", "", ""]
-  ];
-  
-  for(let img of imgs) {
-    
-  }
-  
+function createImgDiv(img) {
+  let container = document.createElement("div");
 }
 
+function swap(arr, i, j) {
+  let tmp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = tmp;
+}
 
-function getRandomFreePos(bo) {
+function shuffle(a) {
+    var j, x, i;
   
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+  
+    return a;
+}
+
+function takeRandomPos () {
+  let coordX = Math.floor(Math.random() * (4) + 1);
+  let coordY = Math.floor(Math.random() * (4) + 1);
+  retunr
 }
