b1d5faa17c0b0e60012ac646c0df0f85f96e8515
Glitch (hello-webpage) <none>
2020-07-29 10:33:18 +0000

    👾🐧 Checkpoint ./style.css:18108127/26:18108132/235:18108380/56 ./index.html:18108132/74 ./script.js:18108132/885

---
+++ script.js
@@ -0,0 +1,36 @@
+let imgSrc = [
+  { 
+    pairId: 0,
+    src: "patata",
+  }, 
+  { 
+    pairId: 1,
+    src: "patata",
+  }, 
+  { 
+    pairId: 2,
+    src: "patata",
+  }, 
+];
+
+
+
+
+function createImgsGrid(imgs) {
+  let board = [
+    ["", "", "", ""],
+    ["", "", "", ""],
+    ["", "", "", ""],
+    ["", "", "", ""]
+  ];
+  
+  for(let img of imgs) {
+    
+  }
+  
+}
+
+
+function getRandomFreePos(bo) {
+  
+}
